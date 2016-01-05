dd941a5a64c9b1169ee0ca30d0215504c9434614
Jérémie Marc <jeremie.marc@yahoo.com>
2016-01-05 17:56:45 +0100

    Adding rotating cards

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
+/* jshint node:true */
+
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
-var sh = require('shelljs');
+var jshint = require('gulp-jshint');
+
+var JS_FILES = ['./www/js/**/*.js'];
 
 var paths = {
   sass: ['./scss/**/*.scss']
 };
+
+gulp.task('jshint', function() {
+    return gulp.src(JS_FILES)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
 
 gulp.task('default', ['sass']);
 
@@ -28,24 +36,5 @@ gulp.task('sass', function(done) {
 
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
-});
-
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
-});
-
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
+  gulp.watch(JS_FILES, ['jshint']);
 });
